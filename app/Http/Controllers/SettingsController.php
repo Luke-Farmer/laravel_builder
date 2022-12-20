016e10a98161c175f1b842e0bd935fd1509bb385
SirGandhi <it4luke@outlook.com>
2022-12-20 14:11:50 +0000

    Tidy up

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -57,7 +57,7 @@ class SettingsController extends Controller
         $theme->active = 0;
         $theme->save();
         $newTheme = $request->input('theme');
-        $updateTheme = Themes::where('theme_name', '=', '$newTheme');
+        $updateTheme = Themes::where('theme_name', '=', $newTheme);
         $updateTheme->active = 1;
 
 
