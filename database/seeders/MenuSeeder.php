09cf410f5f6f55dbffc79a2eec5c35acaba89a68
SirGandhi <it4luke@outlook.com>
2023-01-05 16:04:19 +0000

    tests

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-use App\Models\Customers;
+use App\Models\Menu;
 
 class CustomersSeeder extends Seeder
 {
