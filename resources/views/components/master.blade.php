ac53fb2798ff5b5fbaaf3e75d5432cb9a82abe22
SirGandhi <it4luke@outlook.com>
2023-01-06 15:27:13 +0000

    tests

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -85,6 +85,8 @@ $navigation = $navigationService->getNavigation();
                                     .navbar .nav-item:hover .dropdown-menu{ display: block; }
                                     .navbar .nav-item .dropdown-menu{ margin-top:0; }
                                     .navbar .nav-item { position: relative; }
+                                    .navbar .nav-item .dropdown-menu { margin-left: 100%; }
+                                    .navbar .nav-item .menu-level-1 { margin-left: 0!important; }
                                 }
                             </style>
                             <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
