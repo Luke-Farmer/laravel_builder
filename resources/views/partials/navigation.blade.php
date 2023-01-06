c5783358054b797abe280bee9ac7d4edb3fa4279
SirGandhi <it4luke@outlook.com>
2023-01-06 15:20:39 +0000

    tests

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,9 +1,11 @@
-<ul class="dropdown-menu">
+<?php $count = $count ?? 1; ?>
+<ul class="dropdown-menu0-{{ strval($count) }}">
     @foreach($navigation as $link)
         <li class="nav-item">
             <a href="{{ $link['url'] }}" class="dropdown-item">{{ $link['text'] }}</a>
             @if(!empty($link['children']))
-                @include('partials.navigation', ['navigation' => $link['children']])
+                <?php $count++; ?>
+                @include('partials.navigation', ['navigation' => $link['children'], ['count' => $count]])
             @endif
         </li>
     @endforeach
