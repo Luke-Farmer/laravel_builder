1951851a1f5641d1cacaca7e3e3a17fd1f413bba
SirGandhi <it4luke@outlook.com>
2023-01-10 13:56:18 +0000

    tests

--- resources/views/navigation/index.blade.php
+++ resources/views/navigation/index.blade.php
@@ -9,15 +9,26 @@
             <div class="col-12 p-3">
                 <div class="row">
                     <ul>
-                        @foreach($categories as $category)
-                            <li>{{ $category->name }}</li>
-                            <ul>
-                                @foreach($nav as $nav_item)
-                                    @if($nav_item->category_id == $category->id)
-                                        <li>{{ $nav_item->name }} - {{ $nav_item->url }}</li>
-                                    @endif
-                                @endforeach
-                            </ul>
+                        @foreach($navigation as $link)
+                            <li class="nav-item">
+                                @if(empty($link['children']))
+                                    <a href="{{ $link['url'] }}" class="nav-link">{{ $link['text'] }}</a>
+                                @else
+                                    <a href="{{ $link['url'] }}" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" id="dropdown-{{ $link['text'] }}">{{ $link['text'] }}</a>
+                                @endif
+                                @if(!empty($link['children']))
+                                    <ul class="dropdown-menu ms-0" aria-labelledby="dropdown-{{ $link['text'] }}">
+                                        @foreach($link['children'] as $child)
+                                            <li class="nav-item">
+                                                <a href="{{ $child['url'] }}" class="dropdown-item" id="dropdown-{{ $link['text'] }}">{{ $child['text'] }}</a>
+                                                @if(!empty($child['children']))
+                                                    @include('partials.navigation', ['navigation' => $child['children']])
+                                                @endif
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                @endif
+                            </li>
                         @endforeach
                     </ul>
                 </div>
