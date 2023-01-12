2aa5b719efbc962a4edf01242e03874a24cc83c3
SirGandhi <it4luke@outlook.com>
2023-01-12 16:14:05 +0000

    tests

--- routes/web.php
+++ routes/web.php
@@ -70,6 +70,6 @@ Route::get('/admin/users/', [AdminController::class, 'usersIndex'])->name('users
 
 Route::get('/admin/profile/', [AdminController::class, 'userProfile'])->name('users.profile');
 
-Route::get('/{slug}/*', [PageController::class, 'getPage']);
+Route::get('/{slug}', [PageController::class, 'getPage']);
 
 require __DIR__.'/auth.php';
