12a409187e5583a6c186d79641e360c5db80c5bf
SirGandhi <it4luke@outlook.com>
2023-05-17 16:56:16 +0100

    tests

--- resources/views/instagram/index.blade.php
+++ resources/views/instagram/index.blade.php
@@ -1,6 +1,7 @@
 <x-admin-master>
     <a href="/instagram-get-auth/">Add Account</a>
     @foreach($feed as $post)
+        @dd($post)
         <img src={{ $post->url }} alt="A post from my instagram">
     @endforeach
 </x-admin-master>
