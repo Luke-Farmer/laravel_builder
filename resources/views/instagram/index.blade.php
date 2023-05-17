e475e2e5b61b8dec691a2b1774be376942fcae3b
SirGandhi <it4luke@outlook.com>
2023-05-17 15:22:12 +0100

    tests

--- resources/views/instagram/index.blade.php
+++ resources/views/instagram/index.blade.php
@@ -1,2 +1,3 @@
-//somewhere in instagram-auth-page.blade.php
-<a class="button-new" href="{{ $instagram_auth_url }}">Click to get Instagram permission</a>
+<x-admin-master>
+    <a class="button-new" href="{{ $instagram_auth_url }}">Click to get Instagram permission</a>
+</x-admin-master>
