d14a78bb85357d7a321613387c954492bef20a0b
SirGandhi <it4luke@outlook.com>
2023-05-17 15:24:37 +0100

    tests

--- app/Http/Controllers/InstagramAuthController.php
+++ app/Http/Controllers/InstagramAuthController.php
@@ -8,7 +8,8 @@ class InstagramAuthController extends Controller
 {
     public function show() {
         $profile = \Dymantic\InstagramFeed\Profile::where('username', 'luke')->first();
+        $feed = \Dymantic\InstagramFeed\Profile::where('username', 'luke')->first()->feed();
 
-        return view('instagram.index', ['instagram_auth_url' => $profile->getInstagramAuthUrl()]);
+        return view('instagram.index', ['instagram_auth_url' => $profile->getInstagramAuthUrl()], ['instagram' => $feed]);
     }
 }
