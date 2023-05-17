919849c5f777d48e56ee2105d2ecdfd7da08b648
SirGandhi <it4luke@outlook.com>
2023-05-17 13:31:30 +0100

    tests

---
+++ resources/views/instagram/index.blade.php
@@ -0,0 +1,14 @@
+<x-admin-master>
+    <div class="p-3">
+        <div class="row g-0">
+            <div class="col-12 d-flex bg-accent-light p-3 w-100">
+                <p class="white mb-0">Instagram</p>
+            </div>
+        </div>
+        <div class="row g-0 bg-white">
+            <div class="col-12 p-3">
+                <a href="#" class="button-new rounded">Conntect Instagram Account</a>
+            </div>
+        </div>
+    </div>
+</x-admin-master>
