4c770c5d171f40b37662d225072dcc2c0b7b3e6c
SirGandhi <it4luke@outlook.com>
2023-05-17 15:35:23 +0100

    tests

--- resources/views/instagram/index.blade.php
+++ resources/views/instagram/index.blade.php
@@ -1,6 +1,6 @@
 <x-admin-master>
     <a class="button-new" href="{{ $instagram_auth_url }}">Click to get Instagram permission</a>
     @foreach($instagram as $post)
-        <img src="{{ $post['url']}}">
+        <img src="{{ $post->url}}">
     @endforeach
 </x-admin-master>
