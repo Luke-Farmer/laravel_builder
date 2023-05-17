d166c9d274c3c018e4b56bb82ac64d8e3d4b0772
SirGandhi <it4luke@outlook.com>
2023-05-17 13:46:21 +0100

    tests

---
+++ resources/views/instagram/defines.php
@@ -0,0 +1,4 @@
+<?php
+    define( 'INSTAGRAM_APP_ID', '146737561623616' );
+    define( 'INSTAGRAM_APP_SECRET', '0492402701a2146175c96ebd1de80f2f' );
+    define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://lukefarmer.uk/admin' );
