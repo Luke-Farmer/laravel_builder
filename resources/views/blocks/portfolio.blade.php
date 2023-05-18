cc11d280ec0e8be759163594f70d7363968e8e9a
SirGandhi <it4luke@outlook.com>
2023-05-18 14:18:20 +0100

    tests

--- resources/views/blocks/portfolio.blade.php
+++ resources/views/blocks/portfolio.blade.php
@@ -1,8 +1,8 @@
 <?php $items = \App\Models\Portfolio::latest()->get(); ?>
 <div class="col-12">
-    <div class="row">
+    <div class="row portfolio-wrap">
         @foreach($items as $item)
-            <div class="col-12 mb-3 mb-lg-0">
+            <div class="col-12 ">
                 <div class="portfolio-item-wrapper position-relative d-flex">
                     <img src="{{ $item->image }}" class="rounded" style="width: 275px;height: 150px;object-fit:cover;"/>
                     <div class="portfolio-item-content ps-4">
@@ -16,3 +16,11 @@
         @endforeach
     </div>
 </div>
+<style>
+    .portfolio-wrap .col-12 {
+        margin-bottom: 1.5rem;
+    }
+    .portfolio-wrap .col-12:last-child {
+        margin-bottom: 0!important;
+    }
+</style>
