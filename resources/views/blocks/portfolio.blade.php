804fbc9f91b432483a7ff597e4ba9d4157e9d684
SirGandhi <it4luke@outlook.com>
2023-05-18 14:10:06 +0100

    tests

--- resources/views/blocks/portfolio.blade.php
+++ resources/views/blocks/portfolio.blade.php
@@ -2,9 +2,9 @@
 <div class="col-12">
     <div class="row">
         @foreach($items as $item)
-            <div class="col-12 col-lg-4 mb-3 mb-lg-0">
+            <div class="col-12 mb-3 mb-lg-0">
                 <div class="portfolio-item-wrapper position-relative">
-                    <img src="{{ $item->image }}" class="w-100" style="height: 416px;object-fit:cover;"/>
+                    <img src="{{ $item->image }}" class="w-100" style="width: 275px;object-fit:cover;"/>
                     <div class="portfolio-item-content position-absolute p-4">
                         <div class="portfolio-item-content-wrap p-5">
                             <p class="white fs-4">{{ $item->title }}</p>
