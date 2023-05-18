f139823f279213f94d019c5992bb5fdf84dcee61
SirGandhi <it4luke@outlook.com>
2023-05-18 14:15:52 +0100

    tests

--- resources/views/blocks/portfolio.blade.php
+++ resources/views/blocks/portfolio.blade.php
@@ -4,7 +4,7 @@
         @foreach($items as $item)
             <div class="col-12 mb-3 mb-lg-0">
                 <div class="portfolio-item-wrapper position-relative d-flex">
-                    <img src="{{ $item->image }}" class="" style="width: 275px;object-fit:cover;"/>
+                    <img src="{{ $item->image }}" class="rounded" style="width: 275px;height: 150px;object-fit:cover;"/>
                     <div class="portfolio-item-content ps-4">
                         <div class="portfolio-item-content-wrap">
                             <p class="white fs-4">{{ $item->title }}</p>
