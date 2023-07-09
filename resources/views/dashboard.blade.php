0595cb845d0f3b6b4304cc4e20210dad095f3d68
SirGandhi <it4luke@outlook.com>
2023-07-09 22:45:07 +0100

    tests

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -75,24 +75,19 @@
                 </div>
             </div>
             <div class="col-12 col-lg-3">
-                <form method="POST" action="/admin/dashboard" id="contact-form" enctype="multipart/form-data">
-                    @csrf
-                    <div class="col-12">
-                        <div class="form-group">
-                            <label class="mb-2">Task Name</label>
-                            <input type="text" class="edit-page-input p-1" name="name"/>
+                <?php $list = \App\Models\Todo::all() ?>
+                @foreach($list as $item)
+                    <li class="bg-white mb-5" style="list-style: none;">
+                        <strong>{{ $item->name }} - </strong>{{ $item->body }}
+                        <div class="d-flex">
+                            <form class="mb-0 w-100 mt-3" action="" method="POST">
+                                @csrf
+                                <input value="{{ $item->name }}" type="text" name="name" class="d-none"/>
+                                <input class="white main-button-light-bg py-1" value="Delete" name="delete" type="submit" onclick="var result = confirm('Want to delete?');">
+                            </form>
                         </div>
-                    </div>
-                    <div class="col-12">
-                        <div class="form-group">
-                            <label class="mb-2">Task Description</label>
-                            <textarea name="body" class="edit-page-input p-1"></textarea>
-                        </div>
-                    </div>
-                    <div class="col-12 mt-3">
-                        <button type="submit" name="create" class="white main-button-light-bg py-1">Add</button>
-                    </div>
-                </form>
+                    </li>
+                @endforeach
             </div>
         </div>
     </div>
