a480b3d8541c173a2f4189d960e54d2d8ac1e443
SirGandhi <it4luke@outlook.com>
2023-07-09 22:28:42 +0100

    tests

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -53,13 +53,13 @@
                         new Chart(document.getElementById("line-chart"), {
                             type : 'line',
                             data : {
-                                labels : [ 1500, 1600, 1700, 1750, 1800, 1850,
+                                labels : [ 'January', 'Febuary', 'March', 1750, 1800, 1850,
                                     1900, 1950, 1999, 2050 ],
                                 datasets : [
                                     {
                                         data : [ 186, 205, 1321, 1516, 2107,
                                             2191, 3133, 3221, 4783, 5478 ],
-                                        label : "America",
+                                        label : "Total Page Views",
                                         borderColor : "#3cba9f",
                                         fill : false
                                     }]
