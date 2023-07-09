3969e5394cad6e538974f31b31dfaa44a8612e56
SirGandhi <it4luke@outlook.com>
2023-07-09 19:01:56 +0100

    tests

--- resources/views/analytics/index.blade.php
+++ resources/views/analytics/index.blade.php
@@ -1,5 +1,5 @@
 <x-admin-master>
-    @dd($analyticsData)
+    <?php dd($analyticsData); ?>
     <div class="p-3" style="background: #D9D9D6;">
         <div class="row g-0">
             <div class="col-4 d-flex bg-dark-blue p-3">
