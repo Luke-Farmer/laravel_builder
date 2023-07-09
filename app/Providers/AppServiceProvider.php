3f648b8e15ae22c317f839d90d9827b975527254
SirGandhi <it4luke@outlook.com>
2023-07-09 13:52:08 +0100

    tests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use Session;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,5 +31,10 @@ class AppServiceProvider extends ServiceProvider
         View::composer('pages.edit', function ($view) {
             Session::forget('success');
         });
+    }
+
+    public function boot()
+    {
+        Paginator::useBootstrap();
     }
 }
