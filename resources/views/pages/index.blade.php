dbad6dd9419f881bcd518766e7fb63060207b0d5
SirGandhi <it4luke@outlook.com>
2023-07-09 12:57:27 +0100

    tests

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,5 @@
 <x-admin-master>
-    <div class="p-3" style="background: #040D1B;">
+    <div class="p-3 ps-0" style="background: #040D1B;">
         <div class="row g-0">
             <div class="col-12 d-flex p-3 w-100 border-top-radius" style="background: linear-gradient(144.39deg, #ffffff -278.56%, #6d6d6d -78.47%, #11101d 91.61%); border-radius: 10px;">
                 <p class="white mb-0">All Pages</p>
