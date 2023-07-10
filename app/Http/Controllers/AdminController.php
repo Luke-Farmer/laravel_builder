093bedc154664327e682a21cc98045707f4871c7
SirGandhi <it4luke@outlook.com>
2023-07-10 17:21:49 +0100

    tests

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -26,7 +26,7 @@ class AdminController extends Controller
 
         $date = Carbon::now();
         $dates[] = array();
-        for($i = 0; $i < 13;) {
+        for($i = 0; $i < 14;) {
             $dates[$i] =  Carbon::now()->subDay($i)->format('d M');
             $i++;
         }
