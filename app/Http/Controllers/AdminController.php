585ea645fa8e23cd5bfd6541c95ca5541a78d87b
SirGandhi <it4luke@outlook.com>
2023-07-11 05:05:15 +0100

    tests

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -19,6 +19,10 @@ use Auth;
 class AdminController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     public function dashboard() {
         $twoWeeks = Analytics::fetchVisitorsAndPageViews(Period::days(14));
