65044fb4e274ac0cca8ad039aad363e70ce796c2
SirGandhi <it4luke@outlook.com>
2023-07-11 05:34:06 +0100

    tests

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -36,7 +36,7 @@ class AdminController extends Controller
         }
 
         $graphData = array();
-        for($i = 0; $i < 13;) {
+        for($i = 0; $i < 2;) {
             $periodDate = Carbon::now()->subDay($i+ 1);
             $periodEnd = Carbon::now()->subDay($i);
             $graphData[$i] = Analytics::fetchVisitorsAndPageViews(Period::create($periodDate, $periodEnd));
