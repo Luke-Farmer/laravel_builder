cea1e1e4965bdb3a7bb05616192ae26f92294da6
SirGandhi <it4luke@outlook.com>
2023-07-11 22:32:23 +0100

    tests

--- resources/views/pages/template.blade.php
+++ resources/views/pages/template.blade.php
@@ -6,7 +6,7 @@
         {!! str_replace(array('[contact_form]', '[latest_portfolios]', '[footer_overlay]', '[portfolios]'), array(view('forms.contact-form'), view('blocks.latest-portfolios'), view('blocks.footer-get-in-touch'), view('blocks.portfolio')) ,$page->body) !!}
     @else
         {!! $page->html !!}
-        {!! $page->css !!}
+        {{ $page->css }}
     @endif
 </x-master>
 <style>
