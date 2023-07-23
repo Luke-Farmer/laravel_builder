870984a075b41e3cdfc07a78b518d06fb2a6ab03
SirGandhi <it4luke@outlook.com>
2023-07-23 17:16:52 +0100

    tests

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -39,7 +39,7 @@
                 <div class="d-flex p-3 d-flex flex-column white" style="background: linear-gradient(144.39deg, #ffffff -278.56%, #6d6d6d -78.47%, #11101d 91.61%);border-radius:10px;">
                     <p class="fs-5 fw-bold">Total Visitors</p>
                     @php
-                        $allVisitors = $usersTwoWeeks[0]['activeUsers'] + $usersTwoWeeks[1]['activeUsers'];
+                        $allVisitors = $usersTwoWeeks[0]['activeUsers'] ?? 0 + $usersTwoWeeks[1]['activeUsers'] ?? 0;
                     @endphp
                     <p class="fs-3 fw-bold">{{ $allVisitors }}</p>
                     <div class="d-flex">
